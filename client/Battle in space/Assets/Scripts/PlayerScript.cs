dd6f58c523a258f76b9dde5d478ca7e5140831a1
Kiryanenko <KiryanenkoAV@mail.ru>
2016-07-26 21:49:31 +0300

    Add controller, but with bugs

--- client/Battle in space/Assets/Scripts/PlayerScript.cs
+++ client/Battle in space/Assets/Scripts/PlayerScript.cs
@@ -1,43 +1,28 @@
 ﻿using UnityEngine;
+using CnControls;
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-    public Vector2 speed = new Vector2(10, 10);
-    private Vector2 movement;
+	private ShipScript _shipScript;
 
 	// Use this for initialization
 	void Start () {
-	
+		_shipScript = GetComponent<ShipScript>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-
-        movement = new Vector2(
-            speed.x * inputX,
-            speed.y * inputY);
-
-        // 5 - Стрельба
-        bool shoot = Input.GetButtonDown("Fire1");
-        shoot |= Input.GetButtonDown("Fire2");
-        // Замечание: Для пользователей Mac, Ctrl + стрелка - это плохая идея
-
-        if (shoot)
-        {
-            WeaponScript weapon = GetComponent<WeaponScript>();
-            if (weapon != null)
-            {
-                // ложь, так как игрок не враг
-                weapon.Attack(false);
-            }
-        }
+		// Передвижение
+        float inputX = CnInputManager.GetAxis("Horizontal");
+        float inputY = CnInputManager.GetAxis("Vertical");
+        
+        _shipScript.Traction = Mathf.Sqrt(inputX * inputX + inputY * inputY);
+		if (inputX != 0 && inputY != 0) _shipScript.finalAngle = Mathf.Atan2(inputX, -inputY) * 180 / Mathf.PI + 180;
 	}
 
     void FixedUpdate()
     {
-        GetComponent<Rigidbody2D>().velocity = movement;
+    
     }
 }
