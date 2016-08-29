2717eebccb1608a49c80f9644fa28309945529bc
Kiryanenko <KiryanenkoAV@mail.ru>
2016-08-29 21:37:51 +0300

    loopedMap

---
+++ client/Battle in space/Assets/Scripts/LoopedMap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopedMap : MonoBehaviour {
+
+	public int size = 1000;
+	public bool isClone = false;
+
+	void Awake()
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (!isClone)
+		{
+			float z = transform.position.z;
+			for (int i = -1; i <= 1; i++)
+				for (int j = -1; j <= 1; j++)
+				{
+					if (i != 0 || j != 0)
+					{
+						var newMap = Instantiate(gameObject);
+						newMap.transform.position = new Vector3(i * size, j * size, z);
+						newMap.GetComponent<LoopedMap>().isClone = true;
+					}
+				}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ client/Battle in space/Assets/Scripts/MoveInLoopedMap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveInLoopedMap : MonoBehaviour {
+	private int _size;
+	// Use this for initialization
+	void Start () {
+		_size = GetComponentInParent<LoopedMap>().size;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		if (Mathf.Abs(pos.x) > _size / 2) pos.x -= pos.x > 0 ? _size : -_size;
+		if (Mathf.Abs(pos.y) > _size / 2) pos.y -= pos.y > 0 ? _size : -_size;
+		transform.position = pos;
+	}
+}
